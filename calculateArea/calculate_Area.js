bab0878905825960076fe086d73314c2c8bfb07d
cristian12972006 <ccrodriguezm@uqvirtual.edu.co>
2025-03-16 12:27:09 -0500

    calcular total compras

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -10,3 +10,20 @@ function calculateArea(){
     document.getElementById('result').innerText=`The area of the rectangule is: ${area}`;
 
 }
+
+
+let art1;
+let art2;
+let art3;
+function groceryTracker(){
+
+    art1=parseFloat(document.getElementById('num1').value);
+    art2=parseFloat(document.getElementById('num2').value);
+    art3=parseFloat(document.getElementById('num3').value);
+
+    let total= art1+ art2+ art3;
+
+    document.getElementById('result1').innerText=`total de compras es: ${total}`;
+
+
+}
